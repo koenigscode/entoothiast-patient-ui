9237d1cc2eb394f1eebf94454791ef2b80b7f650
mitac <mitac@student.chalmers.se>
2023-11-14 01:12:32 +0100

    Add all dentists page

---
+++ my-app/src/views/AllDentists.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="list-of-dentists">
+      <div class="div-2">
+        <img class="logo" alt="Logo" src="../assets/entoothiast.png" />
+        <div class="box">
+    <div class="rectangle" />
+    <div class="label">
+    <div class="text-wrapper">All dentists</div>
+    <div class="label2">
+    <div class="text-wrapper2">Dentists</div>
+
+  </div>
+</div>
+</div>
+</div>
+</div>
+</template>
+  
+  <script>
+  
+  export default {
+    name: "ListOfDentists",
+  };
+  </script>
+  
+  <style>
+ @import url("../assets/styles/allDentists-style.css");
+  </style>
+  
