0606849734688294b9983f71b7f48f0ccec0cb07
Anna <annamak@student.chalmers.se>
2023-11-14 22:34:13 +0100

    Add patient settings page #6

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Login from './views/Login.vue';
 import Home from './views/Home.vue';
+import Settings from './views/Settings.vue';
 
 const routes = [
   {
@@ -13,6 +14,11 @@ const routes = [
     name: 'Login-page',
     component: Login,
   },
+  {
+    path: '/settings',
+    name: 'Settings-page',
+    component: Settings,
+  }
 ];
 
 const router = createRouter({
---
+++ src/views/Settings.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="container">
+      <h1>Settings</h1>
+      <div class="columns">
+        <div class="half-column">
+            <p><b>Change password</b></p>
+            <p>New password</p>
+            <div class="input-field"></div>
+
+            <p>New password again</p>
+            <div class="input-field"></div>
+
+            <p><b>Notifications</b></p>
+            <div class="columns">
+                <div class="on">On</div>
+                <div class="off">Off</div>
+            </div>
+        </div>
+
+        <div class="half-column">
+            <p><b>Change email</b></p>
+            <p>New email</p>
+            <div class="input-field"></div>
+
+            <p>New email again</p>
+            <div class="input-field"></div>
+
+            <p><b>Dark mode</b></p>
+            <div class="columns">
+                <div class="off">On</div>
+                <div class="on">Off</div>
+            </div>
+        </div>
+    </div>
+
+    <button class="primary-btn">Save</button>
+    <button class="secondary-btn">Back</button>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Settings-page",
+};
+</script>
+
+<style>
+@import url("../assets/styles/style.css");
+</style>
