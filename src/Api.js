7b77b02dba5ea263e716db613412e81a5aaf7450
mitac <mitac@student.chalmers.se>
2023-11-14 17:10:07 +0100

    Update Login page, router.js and main.js

---
+++ src/Api.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-undef */
+import axios from "axios";
+
+export const Api = axios.create({
+	baseURL: process.env.VUE_APP_API_ENDPOINT || "http://localhost:3000/"
+});
---
+++ src/main.js
@@ -0,0 +1,22 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router'; // Import router configuration
+
+// Import PrimeVue styles
+import 'primevue/resources/primevue.min.css'; // core css
+import 'primeicons/primeicons.css';
+
+// Import PrimeVue components
+import Button from 'primevue/button';
+
+const app = createApp(App);
+
+// Use PrimeVue components globally
+// eslint-disable-next-line vue/multi-word-component-names
+app.component('Button', Button);
+
+// Use Vue Router
+app.use(router);
+
+app.mount('#app');
+
