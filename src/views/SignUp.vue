9a407256e6c58311048bf5852231ca4e0c02ee3a
mitac <mitac@student.chalmers.se>
2023-11-27 03:59:52 +0100

    Add sign up page and styling  #3

---
+++ src/views/SignUp.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+      <div class="login-container">
+          <h1>Create an account</h1>
+          <p>Name</p>
+            <div class="input-field"></div>
+          <p>Email</p>
+            <div class="input-field"></div>
+          <p>Password</p>
+            <div class="input-field"></div>
+          <p>Re-type password</p>
+            <div class="input-field"></div>
+          <button class="primary-btn">Submit</button>
+        </div> 
+        <div class="image-container">
+      <img src="../assets/login-img.png" class="centered-image">
+    </div>
+      </div> 
+  </template>
+    
+    <script>
+    export default {
+      name: "SignUp-page",
+    };
+    </script>
+    
+    <style>
+    @import url("../assets/styles/login-style.css");
+    </style>
+    
