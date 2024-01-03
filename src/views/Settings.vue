2689a04f0d50bef1910e747423076ea7bfa798a7
Anna <annamak@student.chalmers.se>
2024-01-03 22:14:22 +0100

    Fix styling

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -2,50 +2,28 @@
   <body>
     <img src="../assets/entoothiast.png" class="logo">
     <div class="container">
-      <div class="iconbar">
-        <img src="../assets/notification.png" class="icon">
-        <img src="../assets/notification.png" class="icon">
-        <img src="../assets/settings.png" class="icon">
-        <img src="../assets/logout.png" class="icon">
-      </div>
       <h1>Settings</h1>
       <div class="columns">
         <div class="half-column">
+            <p><b>Change name</b></p>
+            <input class="input-field" type="name" placeholder="Enter new name" v-model="name" required />
+
             <p><b>Change password</b></p>
-            <p>New password</p>
             <input class="input-field" type="password" placeholder="Enter new password" v-model="password" required />
-            <input class="input-field" type="password" placeholder="Enter new password" v-model="password" required />
-
-            <p>New password again</p>
-            <input class="input-field" type="password" placeholder="Confirm new password" v-model="repeatedPassword" required />
-            <input class="input-field" type="password" placeholder="Confirm new password" v-model="repeatedPassword" required />
-
-            <p><b>Notifications</b></p>
-            <div class="columns">
-              <div class="on" @click="toggleNot(false)" :class="{ off: !isNotification }">On</div>
-              <div class="on" @click="toggleNot(true)" :class="{ off: isNotification }">Off</div>
-            </div>
         </div>
 
         <div class="half-column">
             <p><b>Change username</b></p>
-            <p>New username</p>
-            <input class="input-field" type="username" placeholder="Enter new username" v-model="username" required />
-            <p><b>Change username</b></p>
-            <p>New username</p>
             <input class="input-field" type="username" placeholder="Enter new username" v-model="username" required />
 
-            <p><b>Dark mode</b></p>
-            <div class="columns">
-              <div class="on" @click="toggleDarkMode(false)" :class="{ off: !darkMode }">On</div>
-              <div class="on" @click="toggleDarkMode(true)" :class="{ off: darkMode }">Off</div>
-            </div>
+            <p><b>Confirm password</b></p>
+            <input class="input-field" type="password" placeholder="Confirm new password" v-model="confirmPassword" required />
         </div>
     </div>
 
     <div class="columns">
       <button class="primary-btn" @click="updateInfo">Save</button>
-      <RouterLink to="/" class="secondary-btn">Back</RouterLink>
+      <button class="secondary-btn" @click="goBack">Back</button>
     </div>
     <p v-if="errorMessage" class="error-message">{{ errorMessage }}</p>
     <p v-if="successMessage" class="success-message">{{ successMessage }}</p>
@@ -53,12 +31,9 @@
 
     </div>
   </body>
-    </div>
-  </body>
 </template>
 
 <script>
-import { RouterLink } from 'vue-router';
 import {Api} from '../Api';
 
 export default {
@@ -66,44 +41,61 @@ export default {
     data() {
         return {
             username: '',
+            name: '',
             password: '',
-            darkMode: false,
-            isNotification: true,
+            confirmPassword: '',
+            errorMessage: '',
+            successMessage: '',
         };
     },
+  
     methods: {
-        updateInfo() {
-            this.errorMessage = '';
-            this.successMessage = '';
-            if (this.password !== this.repeatedPassword) {
-                this.errorMessage = "Passwords do not match";
-                return;
-            }
-            const userData = {
-                name: this.name,
-                username: this.username,
-                password: this.password,
-                role: "patient"
-            };
-            console.log(Api);
-            Api.put('/v1/users/register', userData)
-                .then(response => {
-                console.log(response.data);
-                this.successMessage = "Infor updated.";
-            })
-                .catch(error => {
-                console.error(error.response.data);
-                this.errorMessage = "Error during update. Please try again.";
-            });
-        },
+      updateInfo() {
+      this.errorMessage = '';
+      this.successMessage = '';
+
+      if (this.password !== this.confirmPassword) {
+      this.errorMessage = 'Password and Confirm Password must match.';
+      return;
+    }
+      const userData = {
+        name: this.name,
+        username: this.username,
+        password: this.password
+      };
+
+      const userId = localStorage.getItem('userId');
+
+      if (!userId) {
+      this.errorMessage = "User ID not found. Please try logging in again.";
+      return;
+      }
+      Api.patch(`/v1/users/${userId}`, userData)
+        .then(response => {
+          console.log(response.data);
+          this.successMessage = "Profile info updated successfully.";
+          
+          // Reset form fields after successful update
+          this.username = "";
+          this.name = "";
+          this.password= "";
+          this.confirmPassword= "";
+        })
+        .catch(error => {
+          console.error(error.response.data);
+          this.errorMessage = "Error during update. Please try again.";
+        });
+    },
         toggleDarkMode(value) {
           this.darkMode = value
         },
         toggleNot(value) {
           this.isNotification = value
+        },
+        goBack() {
+          this.$router.go(-1)
         }
-    },
-    components: { RouterLink }
+    }
 };
 </script>
 
