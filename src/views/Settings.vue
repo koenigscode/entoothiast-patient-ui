da0635ae9b3ed1e075f348b065bdff608759edbc
Isabela Mitac <mitac@student.chalmers.se>
2024-01-08 23:08:20 +0100

    Integrate update password #12

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -6,19 +6,14 @@
       <div class="columns">
         <div class="half-column">
             <p><b>Change name</b></p>
-            
             <input class="input-field" type="name" placeholder="Enter new name" v-model="name" required />
 
-            <p><b>Notifications</b></p>
-            <div class="columns">
-              <div class="on" @click="toggleNot(false)" :class="{ off: !isNotification }">On</div>
-              <div class="on" @click="toggleNot(true)" :class="{ off: isNotification }">Off</div>
-            </div>
+            <p><b>Change password</b></p>
+            <input class="input-field" type="password" placeholder="Enter new password" v-model="password" required />
         </div>
 
         <div class="half-column">
             <p><b>Change username</b></p>
-       
             <input class="input-field" type="username" placeholder="Enter new username" v-model="username" required />
 
             <p><b>Confirm password</b></p>
@@ -47,8 +42,8 @@ export default {
         return {
             username: '',
             name: '',
-            darkMode: false,
-            isNotification: true,
+            password: '',
+            confirmPassword: '',
             errorMessage: '',
             successMessage: '',
         };
@@ -59,9 +54,14 @@ export default {
       this.errorMessage = '';
       this.successMessage = '';
 
+      if (this.password !== this.confirmPassword) {
+      this.errorMessage = 'Password and Confirm Password must match.';
+      return;
+    }
       const userData = {
         name: this.name,
         username: this.username,
+        password: this.password
       };
 
       const userId = localStorage.getItem('userId');
@@ -78,8 +78,8 @@ export default {
           // Reset form fields after successful update
           this.username = "";
           this.name = "";
-          this.darkMode = false;
-          this.isNotification = true;
+          this.password= "";
+          this.confirmPassword= "";
         })
         .catch(error => {
           console.error(error.response.data);
